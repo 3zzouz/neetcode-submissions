8893abd8706ed66b673b25f0cf2bdd61e5f19943
Dhouibi Mohamed Aziz <108880033+3zzouz@users.noreply.github.com>
2026-07-14 12:42:58 +0100

    Add: permutation-string - submission-27

---
+++ Data Structures & Algorithms/permutation-string/submission-27.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        aux = {}
+        n = len(s2)
+        x = len(s1)
+        for c in s1:
+            if c not in aux : aux[c] = 0
+            aux[c]+=1
+        cop = aux.copy()
+        l = 0
+        r = 0
+        while r<n :
+            isskipping = False
+            while r < n and s2[r] not in aux:
+                r+=1
+                isskipping = True
+            if isskipping :
+                aux = cop.copy()
+                l = r
+            if r < n and s2[r] in aux and aux[s2[r]] >= 1:
+                aux[s2[r]]-=1
+                r+=1
+            elif r < n and s2[r] in aux and aux[s2[r]] < 1:
+                aux[s2[l]] += 1
+                l+=1
+            if r-l == x: return True
+        return False
