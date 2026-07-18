66b73491306500736f64397502e71c15052d5ad6
Dhouibi Mohamed Aziz <108880033+3zzouz@users.noreply.github.com>
2026-07-18 19:09:59 +0100

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,12 @@
+from collections import deque
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        aux = deque([])
+        for i,e in enumerate(temperatures):
+            while aux and aux[-1][1] < e:
+                res[aux[-1][0]] = i - aux[-1][0]
+                aux.pop()
+            aux.append((i,e))
+        return res
+            
