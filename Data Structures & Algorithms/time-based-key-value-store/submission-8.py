f9df58cee2a6e9147003a0429a9decdbe374c721
Dhouibi Mohamed Aziz <108880033+3zzouz@users.noreply.github.com>
2026-07-19 16:31:33 +0100

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,27 @@
+from collections import deque, defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.ma : dict[str,deque[tuple[int,str]]] = defaultdict(deque)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.ma[key].append((timestamp,value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        array = self.ma[key]
+        if len(array) ==0 : return ''
+        l = 0
+        r = len(array) - 1
+        while l<r-1 :
+            mil = int((l+r)/2)
+            if array[mil][0] == timestamp : return array[mil][1]
+            if array[mil][0] > timestamp:
+                r = mil - 1
+            else:
+                l = mil
+        print(r)
+        if array[r][0] <= timestamp:
+            return array[r][1]
+        if array[l][0] <= timestamp:
+            return array[l][1]
+        return ''
